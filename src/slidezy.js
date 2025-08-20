434439bcbbe4b4a0287fa8f057e474c8aec32a5d
Your Name <your-email@example.com>
2025-08-20 15:36:05 +0700

    Thêm option items và loop

--- src/slidezy.js
+++ src/slidezy.js
@@ -7,6 +7,7 @@ function Slidezy(selector, options) {
 
     this.opt = Object.assign(
         {
+            item: 1,
             loop: false,
         },
         options
@@ -31,6 +32,7 @@ Slidezy.prototype._createTrack = function () {
 
     this.slides.forEach((slide) => {
         slide.classList.add("slidezy-slide");
+        slide.style.flexBasis = `calc(100% / ${this.opt.item})`;
         this.track.appendChild(slide);
     });
 
@@ -53,10 +55,16 @@ Slidezy.prototype._createNavigation = function () {
 };
 
 Slidezy.prototype.moveSlide = function (step) {
-    this.currentIndex = Math.min(
-        Math.max(this.currentIndex + step, 0),
-        this.slides.length - 3
-    );
-    this.offset = -(this.currentIndex * (100 / 3));
+    if (this.opt.loop) {
+        this.currentIndex =
+            (this.currentIndex + step + this.slides.length) %
+            this.slides.length;
+    } else {
+        this.currentIndex = Math.min(
+            Math.max(this.currentIndex + step, 0),
+            this.slides.length - this.opt.item
+        );
+    }
+    this.offset = -(this.currentIndex * (100 / this.opt.item));
     this.track.style.transform = `translateX(${this.offset}%)`;
 };
